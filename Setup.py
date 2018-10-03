fd5c55c1b25e5ecad1f67f4d1fc89cddf6ea54fb
Thomas Reis <ChazzyDango@users.noreply.github.com>
2018-10-02 20:34:07 -0400

    Added a 10 :(

--- Setup.py
+++ Setup.py
@@ -4,7 +4,7 @@ from Dealer import *
 
 game_mode = GameModes.CLASSIC
 suitsList = ['heart', 'diamond', 'spade', 'clubs']
-valuesList = ['Ace', 2, 3, 4, 5, 6, 7, 8, 9, 'Jack', 'Queen', 'King']
+valuesList = ['Ace', 2, 3, 4, 5, 6, 7, 8, 9, 10, 'Jack', 'Queen', 'King']
 
 
 def play_game(game_deck):
